97538991754c5bd56954c5a37424923dbb700085
Ariful0007 <arifulpub143@gmail.com>
2023-05-08 15:08:24 +0600

    nnn

--- UiTetsTable/SendController.swift
+++ UiTetsTable/SendController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
-class SendController: UIViewController {
 
+
+class SendController: UIViewController {
+    var names = ["IO"]
+
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.delegate = self
+        tableview.dataSource = self
 
        
     }
@@ -18,4 +25,30 @@ class SendController: UIViewController {
 
   
 
+}
+
+
+extension SendController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        print("UI")
+    }
+    
+    
+}
+extension SendController : UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+          
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+      
+    }
+    
 }
