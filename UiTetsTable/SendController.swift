4ed2631bb103d1a053b13032543f30215f482773
Ariful0007 <arifulpub143@gmail.com>
2023-05-08 14:28:46 +0600

    nnn

---
+++ UiTetsTable/SendController.swift
@@ -0,0 +1,21 @@
+//
+//  SendController.swift
+//  UiTetsTable
+//
+//  Created by sang on 17/10/1444 AH.
+//
+
+import UIKit
+
+class SendController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+  
+
+}
--- UiTetsTable/ViewController.swift
+++ UiTetsTable/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+let names = [
+ "Arif","Arif1","Arifu2"]
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
 
 
 }
 
+
+
+
+extension ViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sec = storyboard?.instantiateViewController(identifier: "kik") as! SendController
+                            present(sec,animated: true)
+    }
+    
+}
+extension ViewController : UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+          
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+}
